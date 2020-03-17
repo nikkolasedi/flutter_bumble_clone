15ff8e94e70100917fd07441e43211aabc2854d5
Zonily <zonilyjame@gmail.com>
2020-03-17 13:17:21 +0800

    UItils and Constants

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/rendering.dart';
+
+class Constants {
+  // Colors
+  static Color get bumbleYellow => Color(0xffFFCB37);
+
+  // Urls
+  static String termsAndConditionsUrl = 'https://bumble.com/en-us/terms/';
+  static String privacyPolicyUrl = 'https://bumble.com/en-us/privacy/';
+}
