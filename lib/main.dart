785769e3b31be8a502cec9671c464daf90bd39a6
Zonily <zonilyjame@gmail.com>
2020-03-17 23:34:22 +0800

    Removed unused pubs

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bumble_clone/screens/landing_screen.dart';
-import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // FlutterStatusbarcolor.setStatusBarColor(Colors.white);
-
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
