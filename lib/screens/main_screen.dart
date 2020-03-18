c222213e64f0f2909b65e48c8c6b8ba41711d667
Zonily <zonilyjame@gmail.com>
2020-03-19 01:14:35 +0800

    Dummy main screen

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: Container(),
+    );
+  }
+}
